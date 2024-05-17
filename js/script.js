3afe72269a41909a37bd478c90232e37f460ac57
Bastián <137963120+BastianLazo@users.noreply.github.com>
2024-05-16 21:45:30 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,19 @@ fetch('/js/data.json')
 
     placeholder.innerHTML = out;
 })
+
+function validarRegistro() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (email.trim() === '') {
+        alert('Por favor ingrese su correo electrónico.');
+        return false;
+    }
+
+    if (password.trim() === '') {
+        alert('Por favor ingrese su contraseña.');
+        return false;
+    }
+    return true;
+}

fb457a033b06311f090881142d1b57c78c7c14d4
Bastián <137963120+BastianLazo@users.noreply.github.com>
2024-06-09 17:53:35 -0400

    Add files via upload

--- productos/forms.py
+++ productos/forms.py
@@ -1,13 +1,10 @@
 from django import forms
-
 from .models import productos
-
-
 
 class productosForm(forms.ModelForm):
     class Meta:
         model = productos
-        fields = ['nombre', 'precio', 'imagen', 'categoria']  # fields to be displayed in the form
+        fields = ['nombre', 'precio', 'imagen', 'categoria']
         labels = {
             'nombre': 'Nombre del producto',
             'precio': 'Precio del producto',
--- productos/views.py
+++ productos/views.py
@@ -1,31 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import productos
-# Create your views here.
-
+from .forms import productosForm
 
 def index(request):
-    context={}
+    context = {}
     return render(request, 'productos/index.html', context)
 
 def listar_productos(request, categoria):
     var_productos = productos.objects.filter(categoria=categoria)
-    context={
-        'productos':var_productos,
-        'categoria':categoria,
+    context = {
+        'productos': var_productos,
+        'categoria': categoria,
     }
     return render(request, 'productos/productos.html', context)
 
-
 def agregar_productos(request):
-    context={}
-    return render(request, 'agregarproductos.html', context)
+    if request.method == 'POST':
+        form = productosForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = productosForm()
+    return render(request, 'productos/agregarproductos.html', {'form': form})
+
+def editar_producto(request, id):
+    producto = productos.objects.get(id=id)
+    if request.method == 'POST':
+        form = productosForm(request.POST, request.FILES, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = productosForm(instance=producto)
+    return render(request, 'productos/editar_producto.html', {'form': form})
+
+def eliminar_producto(request, id):
+    producto = productos.objects.get(id=id)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'productos/eliminar_producto.html', {'producto': producto})
 
 def crud(request):
     var_productos = productos.objects.all()
     context = {'productos': var_productos}
-    return render(request,'crud/productos_list.html', context)
+    return render(request, 'crud/productos_list.html', context)
 
 def productosAdd(request):
-    if request.method is not "post":
-        context={}
-        return render(request,'crud/productos_add.html',context)
+    if request.method == "POST":
+        form = productosForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('crud')
+    else:
+        form = productosForm()
+    return render(request, 'crud/productos_add.html', {'form': form})
